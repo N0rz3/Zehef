cdfcbd8e0530a566f09ccdcf4b8c63f229eb93c1
Norze <123885505+N0rz3@users.noreply.github.com>
2023-06-24 12:32:33 +0200

    Update pastebin.py

--- modules/pastebin.py
+++ modules/pastebin.py
@@ -4,27 +4,40 @@ original code => https://github.com/KanekiWeb/Email-Osint/blob/main/modules/Past
 from ScrapeSearchEngine.SearchEngine import Google
 from lib.colors import *
 from lib.request import *
+from lib.objects import *
 
-async def pastebin_check(i, email):
-	try:
-		link = str(i).replace("https://pastebin.com/","https://pastebin.com/raw/")
-		r = Requests(link)
-		data = await r.sender()
+import asyncio
 
-		if email.lower() in data.text.lower() or email in data.text or email.upper() in data.text.upper():
-			print(RED+"[!] "+link)
+async def pastebin_check(i, email, result_links):
+    try:
+        link = str(i).replace("https://pastebin.com/", "https://pastebin.com/raw/")
+        r = Requests(link)
+        data = await r.sender()
 
-	except:
-		print("[!] Your adresse IP has banned, retry tomorrow or change your ip.".replace('!',RED+'!'+WHITE))
+        if email.lower() in data.text.lower() or email in data.text or email.upper() in data.text.upper():
+            result_links.append(link)
+
+    except:
+        print("[!] Your IP address has been banned. Retry tomorrow or change your IP.".replace('!', RED + '!' + WHITE))
 
 async def pastebin_dump(email):
+    search = ("site:pastebin.com \"{}\"".format(email))
+    try:
+        googleText, googleLink = Google(search=search, userAgent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.96 Safari/537.36")
+        scraping_msg = TempPrint("[+] 😴 Dumping...")
+        scraping_msg.TPrint()
 
-	search = ("site:pastebin.com \"{}\"".format(email))
-	try:
-		googleText, googleLink = Google(search=search, userAgent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.96 Safari/537.36")
+        result_links = []
 
-		for i in googleLink:
-			await pastebin_check(i, email)
+        tasks = []
+        for i in googleLink:
+            task = pastebin_check(i, email, result_links)
+            tasks.append(task)
 
-	except:
-		print("[!] 🤔 Error.".replace('!',RED+'!'+WHITE))
+        await asyncio.gather(*tasks)
+
+        for link in result_links:
+            print(RED + "[!] " + link + WHITE)
+
+    except:
+        print("[!] 🤔 Error.".replace('!', RED + '!' + WHITE))
