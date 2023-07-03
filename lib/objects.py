ff296de18ce13671890eea66be66fca9591a0b1a
Norze <123885505+N0rz3@users.noreply.github.com>
2023-07-03 13:14:41 +0200

    Update objects.py

--- lib/objects.py
+++ lib/objects.py
@@ -2,16 +2,13 @@ import time
 
 class TempPrint:
     """
-    Function for print a text for a turn of time
+    Function for printing a text for a certain period of time
     """
-    def __init__(self, 
-                message:str,
-                temp: int = 3,
-                ):
-        self.msg = message
+    def __init__(self, message: str, temp: int = 3):
+        self.msg = message.rstrip()
         self.temp = temp
 
     def TPrint(self):
         print(self.msg, end="", flush=True)
         time.sleep(self.temp)
-        print("\r" + " " * len(self.msg) + "\r", end="", flush=True)
+        print("\r" + " " * (len(self.msg) + 1) + "\r", end="", flush=True)
