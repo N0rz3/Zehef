c72a1d808928b4fa826605a7a8ffefbe123e8d7a
Norze <123885505+N0rz3@users.noreply.github.com>
2023-07-15 20:52:18 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -3,13 +3,13 @@
 color 0A     
 echo Installation requirements                                                                           
                                                                                                                                                                              
-pip install httpx
-pip install holehe
-pip install argparse
-pip install datetime
-pip install pwnedpasswords
-pip install scrape-search-engine
-pip install requests
+pip install httpx==0.24.1
+pip install holehe==1.61
+pip install argparse==1.4.0
+pip install datetime==5.1
+pip install pwnedpasswords==2.0.0
+pip install scrape-search-engine==0.2.2
+pip install requests==2.31.0
 
 if %errorlevel% equ 0 (
     echo [+] Installation successful
