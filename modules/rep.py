89dd4555d9f15c16b4b8f809f180d84083d9fc68
Norze <123885505+N0rz3@users.noreply.github.com>
2023-08-27 22:44:16 +0200

    Update rep.py

--- modules/rep.py
+++ modules/rep.py
@@ -13,39 +13,29 @@ async def check_reputation(email: str):
     try:
         date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        r = Requests(api.emailrep(email))
+        r = Requests(api.reputation(email))
         resp = await r.sender()
         js = json.loads(resp.text)
 
-        if js['details']['malicious_activity'] == True:
-            print(f"\n[{RED}{date}{WHITE}] {RED}Malicious Activity{WHITE}")
+        if js['data']['disposable'] == True:
+            print(f"[{RED}{date}{WHITE}] {RED}Disposable{WHITE}")
         else:
-            print(f"[{GREEN}{date}{WHITE}] {GREEN}No Malicious Activity{WHITE}")
+            print(f"[{GREEN}{date}{WHITE}] {GREEN}No Disposable{WHITE}")
 
-        if js['details']['credentials_leaked'] == True:
-            print(f"[{RED}{date}{WHITE}] {RED}Credentials Leaked{WHITE}")
+        if js['data']['deliverable'] == True:
+            print(f"[{RED}{date}{WHITE}] {RED}Deliverable{WHITE}")
         else:
-            print(f"[{GREEN}{date}{WHITE}] {GREEN}No Credentials Leaked{WHITE}")
+            print(f"[{GREEN}{date}{WHITE}] {GREEN}No Deliverable{WHITE}")
 
-        if js['details']['data_breach'] == True:
-            print(f"[{RED}{date}{WHITE}] {RED}Data Breach{WHITE}")
-        else:
-            print(f"[{GREEN}{date}{WHITE}] {GREEN}No Data Breach{WHITE}")
-
-        if js['details']['spam'] == True:
+        if js['data']['spam'] == True:
             print(f"[{RED}{date}{WHITE}] {RED}Spammer{WHITE}")
         else:
             print(f"[{GREEN}{date}{WHITE}] {GREEN}No Spammer{WHITE}")
 
-        if js['details']['spoofable'] == True:
-            print(f"[{RED}{date}{WHITE}] {RED}Spoofable{WHITE}")
+        if js['data']['spam'] == True:
+            print(f"[{RED}{date}{WHITE}] {RED}Spammer{WHITE}")
         else:
-            print(f"[{GREEN}{date}{WHITE}] {GREEN}No Spoofable{WHITE}")
+            print(f"[{GREEN}{date}{WHITE}] {GREEN}No Spammer{WHITE}")
 
-        if js['details']['days_since_domain_creation'] == True:
-            print(f"[{GREEN}{date}{WHITE}] {GREEN}Day Since Domain Creation: {js['details']['days_since_domain_creation']}")
-
-        print(f"\n[{YELLOW}{date}{WHITE}] Reputation => {js['reputation']}{WHITE}")
-        print(f"[{YELLOW}{date}{WHITE}] Suspicious => {js['suspicious']}{WHITE}\n\n")
     except:
         print(f"[{date}] Error : Ratelimit, please change your ip.\n\n")
