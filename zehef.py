2782d220713cc41a7995430b2ee082c6d6103633
Norze <123885505+N0rz3@users.noreply.github.com>
2024-04-14 11:39:10 +0200

    Delete zehef.py

--- zehef.py
+++
@@ -1,5 +0,0 @@
-if __name__ == '__main__':
-    import sys; sys.dont_write_bytecode = True
-    from lib.helpers import show_banner; show_banner()
-    import asyncio
-    from main import main; asyncio.run(main())
