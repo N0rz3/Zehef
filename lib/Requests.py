5a4a31e9d2ec894bd4d741c1d1c2cd59866c0d06
01100111011011000110100 1011101000110001101101000 <tyrique1.daniel@gmail.com>
2024-06-18 17:16:44 -0600

    init

--- lib/Requests.py
+++ lib/Requests.py
@@ -1,6 +1,7 @@
 import httpx
 import requests
 
+
 class Request:
     def __init__(self, url: str, headers=None, params=None, data=None):
         self.url = url
@@ -13,24 +14,29 @@ class Request:
             async with httpx.AsyncClient() as client:
                 response = await client.get(url=self.url, headers=self.head, params=self.params)
                 return response
-
-        except httpx.HTTPError:
+        except httpx.HTTPError as e:
+            print(f"HTTP error occurred: {e}")
             return httpx.Response(status_code=500)
-        
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return httpx.Response(status_code=500)
+
     async def post(self):
         try:
             async with httpx.AsyncClient() as client:
-                requests = await client.post(url=self.url, data=self.data, headers=self.head, params=self.params)
-
-                return requests
-
-        except httpx.HTTPError:
+                response = await client.post(url=self.url, data=self.data, headers=self.head, params=self.params)
+                return response
+        except httpx.HTTPError as e:
+            print(f"HTTP error occurred: {e}")
+            return httpx.Response(status_code=500)
+        except Exception as e:
+            print(f"An error occurred: {e}")
             return httpx.Response(status_code=500)
 
     def Session(self):
         try:
             session = requests.Session()
             return session
-        
-        except:
-            pass
+        except Exception as e:
+            print(f"An error occurred while creating a session: {e}")
+            return None
