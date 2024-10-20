fdb8286eca9d059ed98ab1b5a88774059155598d
Norze <123885505+N0rz3@users.noreply.github.com>
2024-10-20 17:29:32 +0200

    New module 🔥 : Picsart 🎨

---
+++ modules/accounts/picsart.py
@@ -0,0 +1,22 @@
+from lib.Requests import Request
+from lib.colors import *
+
+async def picsart(target: str):
+
+    params = {
+        'email_encoded': 1,
+        'emails': target
+    }
+
+    r = await Request("https://api.picsart.com/users/email/existence", params=params).get()
+
+    if r.json()['status'] == 'success':
+
+        if r.json()['response'] != '[]':
+            print(f"{GREEN}>{WHITE} Picsart")
+            
+        else:
+            print(f"{RED}>{WHITE} Picsart")
+
+    else:
+        print(f"{RED}>{WHITE} Picsart | Ratelimit")
