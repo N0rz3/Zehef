1b2e6bd1285dc7bfe46e21ec4895546ca045e706
Norze <123885505+N0rz3@users.noreply.github.com>
2024-10-20 11:06:38 +0200

    code clarification

--- lib/Requests.py
+++ lib/Requests.py
@@ -1,7 +1,6 @@
 import httpx
 import requests
 
-
 class Request:
     def __init__(self, url: str, headers=None, params=None, data=None, json=None):
         self.url = url
@@ -15,9 +14,11 @@ class Request:
             async with httpx.AsyncClient() as client:
                 response = await client.get(url=self.url, headers=self.head, params=self.params)
                 return response
+            
         except httpx.HTTPError as e:
             print(f"HTTP error occurred: {e}")
             return httpx.Response(status_code=500)
+        
         except Exception as e:
             print(f"An error occurred: {e}")
             return httpx.Response(status_code=500)
@@ -27,9 +28,11 @@ class Request:
             async with httpx.AsyncClient() as client:
                 response = await client.post(url=self.url, data=self.data, headers=self.head, params=self.params, json=self.json)
                 return response
+            
         except httpx.HTTPError as e:
             print(f"HTTP error occurred: {e}")
             return httpx.Response(status_code=500)
+        
         except Exception as e:
             print(f"An error occurred: {e}")
             return httpx.Response(status_code=500)
@@ -38,6 +41,7 @@ class Request:
         try:
             session = requests.Session()
             return session
+
         except Exception as e:
             print(f"An error occurred while creating a session: {e}")
             return None
