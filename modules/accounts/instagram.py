b109313d3451362c86c10e5170a9503b1321f417
Norze <123885505+N0rz3@users.noreply.github.com>
2024-10-20 11:08:38 +0200

    code clarification

--- modules/accounts/instagram.py
+++ modules/accounts/instagram.py
@@ -2,18 +2,14 @@ from lib.Requests import Request
 from lib.colors import *
 
 async def instagram(target: str):
-    # Step 1: Verify Request to Instagram's Signup Page
     req = await Request("https://www.instagram.com/accounts/emailsignup/").get()
 
-    # Step 2: Retrieve and Validate Cookies
     try:
         csrf_token = req.cookies.get('csrftoken')
     except:
         print(f"{RED}>{WHITE} Instagram / Csrftoken not found")
         pass
 
-
-    # Prepare data for the next request
     data = {
         'email': target,
         'first_name': '',
@@ -21,10 +17,8 @@ async def instagram(target: str):
         'opt_into_one_tap': False
     }
 
-    # Step 3: Make the post request with the csrf_token
     r = await Request("https://www.instagram.com/api/v1/web/accounts/web_create_ajax/attempt/", headers={'x-csrftoken': csrf_token}, data=data).post()
-
-    # Step 4: Handle response and possible errors
+    
     try:
         code = r.json().get('errors', {}).get('email', [{}])[0].get('code')
 
@@ -32,7 +26,9 @@ async def instagram(target: str):
             print(f"{GREEN}>{WHITE} Instagram")
         else:
             print(f"{RED}>{WHITE} Instagram")
+
     except (KeyError, IndexError) as e:
         print(f"{RED}>{WHITE} Instagram error: {str(e)}")
+
     except Exception as e:
         print(f"{RED}>{WHITE} Error: {str(e)}")
